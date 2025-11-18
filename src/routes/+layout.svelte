8841a54439f2acad5debc5348695c620876c84e7
Abdulqudduus Babalola <babalolajnr@gmail.com>
2025-11-18 18:20:43 +0100

    feat: add sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,14 @@
 	import { authStore } from '$lib/stores/auth.store';
 	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import { ModeWatcher } from 'mode-watcher';
-	import { onMount } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 	import type { LayoutData } from './$types';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
-	let { children, data }: { children: any; data: LayoutData } = $props();
+	let { children, data }: { children: Snippet; data: LayoutData } = $props();
 
 	onMount(() => {
 		authStore.initialize();
@@ -20,5 +24,29 @@
 
 <ModeWatcher />
 <QueryClientProvider client={data.queryClient}>
-	{@render children()}
+	<Sidebar.Provider>
+		<AppSidebar />
+		<Sidebar.Inset>
+			<header class="flex h-16 shrink-0 items-center gap-2">
+				<div class="flex items-center gap-2 px-4">
+					<Sidebar.Trigger class="-ml-1" />
+					<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+					<Breadcrumb.Root>
+						<Breadcrumb.List>
+							<Breadcrumb.Item class="hidden md:block">
+								<Breadcrumb.Link href="/">Home</Breadcrumb.Link>
+							</Breadcrumb.Item>
+							<Breadcrumb.Separator class="hidden md:block" />
+							<Breadcrumb.Item>
+								<Breadcrumb.Page>Dashboard</Breadcrumb.Page>
+							</Breadcrumb.Item>
+						</Breadcrumb.List>
+					</Breadcrumb.Root>
+				</div>
+			</header>
+			<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+				{@render children()}
+			</div>
+		</Sidebar.Inset>
+	</Sidebar.Provider>
 </QueryClientProvider>
